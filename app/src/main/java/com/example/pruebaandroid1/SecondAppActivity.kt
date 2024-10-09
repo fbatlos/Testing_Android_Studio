a0fcb46d0cc78fb4494444cfec8f67d33f58ea10
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-10-09 18:13:08 +0200

    Idea login

--- app/src/main/java/com/example/pruebaandroid1/SecondAppActivity.kt
+++ app/src/main/java/com/example/pruebaandroid1/SecondAppActivity.kt
@@ -1,34 +1,20 @@
 package com.example.pruebaandroid1
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class FirstAppActivity : AppCompatActivity() {
+class SecondAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_first_app)
+        setContentView(R.layout.activity_second_app)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val pulsame = findViewById<Button>(R.id.pulsame)
-
-
-        pulsame.setOnClickListener {
-
-
-
-
-        }
     }
-
 }
